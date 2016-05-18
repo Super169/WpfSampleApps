915028d7cf417d205d04a65785bca912953c5c9b
James MA <super169@126.com>
2016-05-18 16:11:23 +0800

    NotifyIcon_2 :  Wake up previous instance if any

--- NotifyIcon_2/App.xaml.cs
+++ NotifyIcon_2/App.xaml.cs
@@ -3,7 +3,11 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Configuration;
 using System.Data;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
@@ -17,8 +21,40 @@ namespace NotifyIcon_2
     {
         Mutex mutex;
 
+        #region "For call previous instance"
+        
+        [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Unicode)]
+        internal struct MyStruct
+        {
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 256)]
+            public string Message;
+        }
+
+        internal const int WM_COPYDATA = 0x004A;
+        [StructLayout(LayoutKind.Sequential)]
+        internal struct COPYDATASTRUCT
+        {
+            public IntPtr dwData;       // Specifies data to be passed
+            public int cbData;          // Specifies the data size in bytes
+            public IntPtr lpData;       // Pointer to data to be passed
+        }
+        [SuppressUnmanagedCodeSecurity]
+        internal class NativeMethod
+        {
+            [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+            public static extern IntPtr SendMessage(IntPtr hWnd, int Msg,
+                IntPtr wParam, ref COPYDATASTRUCT lParam);
+
+
+            [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+            public static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+        }
+
+        #endregion
+
         private System.Windows.Forms.NotifyIcon _notifyIcon;
         private bool _isExit;
+
         private void Application_Startup(object sender, StartupEventArgs e)
         {
 
@@ -27,7 +63,41 @@ namespace NotifyIcon_2
             if (!createdNew)
             {
                 mutex = null;
-                MessageBox.Show("Another instance is already running.");
+
+                IntPtr hTargetWnd = NativeMethod.FindWindow(null, "mainwindow");
+                if (hTargetWnd == IntPtr.Zero)
+                {
+
+                    MessageBox.Show("Windows not found");
+                    return;
+                }
+
+                MyStruct myStruct;
+                myStruct.Message = Constants.MSG_WAKEUP;
+                int myStructSize = Marshal.SizeOf(myStruct);
+                IntPtr pMyStruct = Marshal.AllocHGlobal(myStructSize);
+                try
+                {
+                    Marshal.StructureToPtr(myStruct, pMyStruct, true);
+
+                    COPYDATASTRUCT cds = new COPYDATASTRUCT();
+                    cds.cbData = myStructSize;
+                    cds.lpData = pMyStruct;
+                    // NativeMethod.SendMessage(hTargetWnd, WM_COPYDATA, new IntPtr(), ref cds);
+                    NativeMethod.SendMessage(hTargetWnd, WM_COPYDATA, IntPtr.Zero, ref cds);
+                    // MessageBox.Show("Message sent");
+
+
+                    int result = Marshal.GetLastWin32Error();
+                    if (result != 0)
+                    {
+                        // MessageBox.Show(string.Format("Result not zero: {0}", result));
+                    }
+                }
+                finally
+                {
+                    Marshal.FreeHGlobal(pMyStruct);
+                }
                 Application.Current.Shutdown();
                 return;
             }
--- NotifyIcon_2/MainWindow.xaml.cs
+++ NotifyIcon_2/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -8,6 +9,7 @@ using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
+using System.Windows.Interop;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
@@ -24,5 +26,65 @@ namespace NotifyIcon_2
         {
             InitializeComponent();
         }
+
+        #region "For call up from subsequent execution"
+
+        internal const int WM_COPYDATA = 0x004A;
+
+        [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Unicode)]
+        internal struct MyStruct
+        {
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 256)]
+            public string Message;
+        }
+        [StructLayout(LayoutKind.Sequential)]
+        internal struct COPYDATASTRUCT
+        {
+            public IntPtr dwData;       // Specifies data to be passed
+            public int cbData;          // Specifies the data size in bytes
+            public IntPtr lpData;       // Pointer to data to be passed
+        }
+
+        private IntPtr WndProc(IntPtr hwnd, int msg, IntPtr wParam, IntPtr lParam, ref bool handled)
+        {
+            System.Windows.Forms.Message m = System.Windows.Forms.Message.Create(hwnd, msg, wParam, lParam);
+            if (m.Msg == WM_COPYDATA)
+            {
+                // MessageBox.Show("Message received");
+                // Get the COPYDATASTRUCT struct from lParam.
+                COPYDATASTRUCT cds = (COPYDATASTRUCT)m.GetLParam(typeof(COPYDATASTRUCT));
+
+                // If the size matches
+                if (cds.cbData == Marshal.SizeOf(typeof(MyStruct)))
+                {
+                    // Marshal the data from the unmanaged memory block to a 
+                    // MyStruct managed struct.
+                    MyStruct myStruct = (MyStruct)Marshal.PtrToStructure(cds.lpData,
+                        typeof(MyStruct));
+
+                    // Display the MyStruct data members.
+                    if (myStruct.Message.Equals(Constants.MSG_WAKEUP))
+                    {
+                        ((App)Application.Current).ShowMainWindows();
+                    }
+                    else
+                    {
+                        // TODO: will there be any case that the message is not match?  Bug?
+                        // MessageBox.Show("Message not matched");
+                    }
+                }
+            }
+            return IntPtr.Zero;
+        }
+
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            HwndSource source = HwndSource.FromHwnd(new WindowInteropHelper(this).Handle);
+            source.AddHook(new HwndSourceHook(WndProc));
+        }
+
+#endregion
+
     }
 }
