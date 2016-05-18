e873d86c3baa24ab94134f68a079b5a9f006a2ff
James MA <super169@126.com>
2016-05-18 14:47:39 +0800

    SingleInstance_3 : Single Instance controlled by Mutex (checked by waitOne) - not sure why ReleaseMutex is not required, but it works.

--- SingleInstance_3/App.xaml.cs
+++ SingleInstance_3/App.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 
@@ -13,5 +14,16 @@ namespace SingleInstance_3
     /// </summary>
     public partial class App : Application
     {
+        Mutex mutex = new Mutex(false, "{SingleInstance Mutex}");
+
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            if (!mutex.WaitOne(0, true))
+            {
+                MessageBox.Show("Another instance is already running.");
+                Application.Current.Shutdown();
+                return;
+            }
+        }
     }
 }
